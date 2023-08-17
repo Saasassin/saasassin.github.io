b6c332b27a886e3a10efcf31a62481f4f377ad1e
Roy Russo <royrusso@gmail.com>
2023-08-17 14:27:59 -0400

    ---

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'saasassin', // Usually your GitHub org/user name.
   projectName: 'saasassin.github.io', // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
